1abb65464b2c8d8c591979dd0d1a40470bb16356
Andrew Tran <andrewtran912@gmail.com>
2023-08-01 15:46:53 -0700

    added pandas
    Added pandas to the script to help display the data in a structured format (Data Frame)

--- script.py
+++ script.py
@@ -1,18 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import pandas as pd
 
-# Write a function that takes a character name as input and scrapes their information from the One Piece wiki. 
-# The function should use the requests library to fetch the HTML content and BeautifulSoup to parse it.
-
+# function takes in character name and scrapes information from wiki
 def scrape_char_info(character_name):
     url = f"https://onepiece.fandom.com/wiki/{character_name.replace(' ', '_')}"
     html = requests.get(url)
 
     if html.status_code == 200:
         soup = BeautifulSoup(html.content, "html.parser")
-        # use beautifulsoup to extract relevant info from site
 
+        #extract relevant information from wiki
         affiliation_element = soup.find("div", {"data-source" : "affiliation"})
         if affiliation_element:
             affiliation = affiliation_element.findNext("div").text.strip()
@@ -34,15 +33,15 @@ def scrape_char_info(character_name):
         # return extracted info as a dictionary
         character_info = {
             "Name" : character_name,
+            "Origin" : origin,
             "Affiliation" : affiliation,
-            "Bounty" : bounty,
-            "Origin" : origin
+            "Bounty" : bounty
         }
         return character_info
     else:
         print(f"Error: Could not fetch data for {character_name}")
         return None
-    
+
 def main():
     characters_to_scrape = ["Monkey D. Luffy", "Roronoa Zoro", "Nami", "Usopp", "Sanji", "Tony Tony Chopper", "Nico Robin", "Jinbe", "Franky", "Brook"]
     character_info_list = []
@@ -52,12 +51,18 @@ def main():
         if character_info:
             character_info_list.append(character_info)
 
-    # store info in csv file
+    # store character information in a csv file
     with open("one_piece_characters.csv", "w", newline="", encoding="utf-8") as csvfile:
         fieldnames = character_info_list[0].keys()
         writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(character_info_list)
+
+    # read data from csv file using pandas
+    df = pd.read_csv("one_piece_characters.csv")
+    
+    # display data in structured format (Data Frame)
+    print(df)
 
 if __name__ == "__main__":
     main()
